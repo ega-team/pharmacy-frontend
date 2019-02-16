747d7c7705408c96daf1edfa266a58b1066a5ebe
kakke18 <tanaka-kakeru-xm@ynu.jp>
2019-02-16 20:55:01 +0900

    add: view on function

--- contracts/Hello.sol
+++ contracts/Hello.sol
@@ -7,7 +7,7 @@ contract Hello {
         string[] data;
         address sender;
     }
-    
+
     struct Theme {
         string[] data_header;
         string[] data;
@@ -15,14 +15,10 @@ contract Hello {
         uint reward;
         string[] answer_header;
     }
-    
+
     Theme[] public themes;
     Answer[] public answers;
-  
-    function echo(uint no) public returns (string[] memory) {
-        return themes[no].data_header;
-    }
-  
+
     function defineTheme(
         string[] memory data_header,
         string[] memory data,
@@ -33,19 +29,19 @@ contract Hello {
         themes.push(Theme(data_header,data,specification,reword,answer_header));
         return themes.length;
     }
-  
-    function getTheme(uint theme_id) public returns (Theme memory) {
+
+    function getTheme(uint theme_id) public view returns (Theme memory) {
         return themes[theme_id];
     }
-     
-    function getData(uint theme_id) public returns (string[] memory) {
+
+    function getData(uint theme_id) public view returns (string[] memory) {
         return themes[theme_id].data;
     }
-    
+
     function postAnswer(uint theme_id, string[] memory data) public {
         answers.push(Answer(theme_id, data, msg.sender));
     }
-    
+
     function changeReward(uint theme_id, uint new_reward) public {
         themes[theme_id].reward = new_reward;
     }
