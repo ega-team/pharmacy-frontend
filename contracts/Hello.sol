554b0fa9d3d6234427ef624498e441ee9ac0f363
kakke18 <tanaka-kakeru-xm@ynu.jp>
2019-02-17 02:56:18 +0900

    add: search current answer_hash

--- contracts/Hello.sol
+++ contracts/Hello.sol
@@ -2,22 +2,24 @@ pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
 contract Hello {
-    struct Answer {
-        uint theme_id;
-        string[] data;
-        address sender;
-    }
-
     struct Theme {
         string[] data_header;
         string[] data;
         string specification;
         uint reward;
         string[] answer_header;
+        uint limited_time;
+    }
+    struct Answer {
+        uint theme_id;
+        string data_hash;
+        string data_secret_hash;
+        address sender;
     }
 
     Theme[] public themes;
     Answer[] public answers;
+    string public current_answer_hash;
 
     function defineTheme(
         string[] memory data_header,
@@ -25,9 +27,28 @@ contract Hello {
         string memory specification,
         uint reword,
         string[] memory answer_header
-    ) public returns (uint) {
-        themes.push(Theme(data_header,data,specification,reword,answer_header));
+    ) public payable returns (uint) {
+        themes.push(Theme(data_header, data, specification, reword, answer_header, now + 1 weeks));
         return themes.length;
+    }
+
+    function changeReward(uint theme_id, uint new_reward) public {
+        themes[theme_id].reward = new_reward;
+    }
+
+    function getAllTheme() public view returns (Theme[] memory) {
+        return themes;
+    }
+
+    function getAceptedTheme() public view returns (Theme[] memory) {
+        Theme[] memory accepted_themes = new Theme[](themes.length);
+        uint index = 0;
+        for (uint i = 0; i < themes.length; i++) {
+            require(themes[i].limited_time > now);
+            accepted_themes[index] = themes[i];
+            index++;
+        }
+        return accepted_themes;
     }
 
     function getTheme(uint theme_id) public view returns (Theme memory) {
@@ -38,12 +59,31 @@ contract Hello {
         return themes[theme_id].data;
     }
 
-    function postAnswer(uint theme_id, string[] memory data) public {
-        answers.push(Answer(theme_id, data, msg.sender));
+    function postAnswer(
+        uint theme_id,
+        string memory data_hash,
+        string memory data_secret_hash
+        ) public {
+        answers.push(Answer(theme_id, data_hash, data_secret_hash, msg.sender));
     }
 
-    function changeReward(uint theme_id, uint new_reward) public {
-        themes[theme_id].reward = new_reward;
+    mapping (string => uint) hashCount;
+    function getCurrentAnswer(uint theme_id) public returns (string memory) {
+        string memory hash = "";
+        uint max = 0;
+        for (uint i = 0; i < answers.length; i++) {
+            if (theme_id == answers[i].theme_id) {
+                hashCount[answers[i].data_hash]++;
+            }
+        }
+        for (uint i = 0; i < answers.length; i++) {
+            if (theme_id == answers[i].theme_id && max < hashCount[answers[i].data_hash]) {
+                max = hashCount[answers[i].data_hash];
+                hash = answers[i].data_hash;
+            }
+        }
+        current_answer_hash = hash;
+        return current_answer_hash;
     }
 }
 
